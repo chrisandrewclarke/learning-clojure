624605abe381584cdd82b2eb9be1fbeb3048f459
chris <chrisandrewclarke@gmail.com>
2016-11-24 15:23:08 +0200

    Fixed test for keys in recommendations-test.clj

--- test/recommendations_test.clj
+++ test/recommendations_test.clj
@@ -4,19 +4,18 @@
             [recommendations]
             [helper.test :as test-helper]))
 
-(defn- get-expected-retrieve-recommendation-output [output]
-  {:relevance (output :relevance)
-   :fields (output :fields)
-   :contentItemId (output :contentItemId)
-   :relationships (output :relationships)})
+(defn- get-expected-retrieve-recommendation-output-keys []
+  [:relevance
+   :fields
+   :contentItemId
+   :relationships])
 
 (deftest test-retrieve-recommendations
   (let [content-items   (content-items/list)
         content-item    (first content-items)
         input           {:content-item-id (content-item :contentItemId)}
         output          (recommendations/retrieve (input :content-item-id))
-        ;expected-output (get-expected-retrieve-recommendation-output output)
-        ]
+        expected-output (get-expected-retrieve-recommendation-output-keys)]
     (is (vector? output))
-    (is (> (count output) 0))))
-    ;(is (test-helper/every-has-values? output expected-output))))
+    (is (> (count output) 0))
+    (is (test-helper/every-has-values? output expected-output))))
